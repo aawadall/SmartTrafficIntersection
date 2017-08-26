38f2238401c91c84e69cd8e8a0209fabc82f17ba
aawadall <aawadall@ualberta.ca>
2017-08-26 15:03:10 -0400

    Added Direction Class implementing IDirection Interface

---
+++ SmartTrafficIntersection/Direction.cs
@@ -0,0 +1,12 @@
+using System;
+namespace SmartTrafficIntersection
+{
+    /* Direction Class, 
+     * typically a simple class containing only a name, 
+     * with no significance yet to real direction  */
+    public class Direction : IDirection
+    {
+        protected string _directionName;
+
+    }
+}
--- SmartTrafficIntersection/Intersection.cs
+++ SmartTrafficIntersection/Intersection.cs
@@ -8,6 +8,7 @@ namespace SmartTrafficIntersection
 	public class Intersection
 	{
 		// An Intersection should have a set of lanes with different directions, and a set of traffic lights controlling them
+		protected List<IDirection> _directions;
 		protected List<ITrafficController> _trafficControllers;
         protected List<Lane> _lanes;
 
