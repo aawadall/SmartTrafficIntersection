810a60bc3096e05f7291749e943ac6c53d8a33b5
aawadall <aawadall@ualberta.ca>
2017-08-26 17:19:47 -0400

    Added Main

---
+++ SmartTrafficIntersection/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SmartTrafficIntersection
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Intersection FourWay = new FourDirectionIntersection();
+        }
+    }
+}
