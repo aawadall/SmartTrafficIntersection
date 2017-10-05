079fb30512451e9810e48fa9240f85a8517b9e51
Ahmed R. Awadallah <aawadall@ualberta.ca>
2017-10-05 18:06:11 -0400

    Update Program.cs

--- SmartTrafficIntersection/Program.cs
+++ SmartTrafficIntersection/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 namespace SmartTrafficIntersection
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
@@ -15,24 +15,14 @@ namespace SmartTrafficIntersection
             Console.WriteLine(String.Format("Initial Wait Time: {0}",intersection.TotalWaitTime()));
             for(int tick=0;tick < lifeTime; tick++)
             {
-                //Console.Write(".");
-                //intersection.AlterFlow(rnd.Next(4),rnd.Next(-10,10),rnd.Next(-10,10));
                 intersection.Control();
                 intersection.Simulate();
-                /* */ 
                 Console.WriteLine(String.Format("[{0}] : WT : {1} : State {2}",
                 tick,
                 intersection.TotalWaitTime(),
                 Convert.ToString(intersection.State(),16)));
-                /* */
-                //WaitTime[tick] = intersection.TotalWaitTime();
             }
             Console.WriteLine("Wait Time Progress");
-            /* 
-            for(int i=0;i<lifeTime;i++)
-                Console.Write(String.Format("\t{0}",WaitTime[i]));
-            Console.WriteLine();
-            */
             intersection.Report();
         }
     }
