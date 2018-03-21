99d869148b330d636bbf619ae6e9b18cefc187c1
aawadall <aawadall@ualberta.ca>
2018-03-20 22:22:21 -0400

    recovery

---
+++ SmartTrafficIntersection/ConfigReader.cs
@@ -0,0 +1,10 @@
+using System;
+namespace SmartTrafficIntersection
+{
+    /* This class is used to configure intersections, based on data read from a file or database */
+    public class ConfigReader
+    {
+
+    }
+
+}
