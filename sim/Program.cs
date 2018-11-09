60f7ebbc8ed403bd846d782500373e96bd0596e9
Ahmed Awadallah <aawadall@ualberta.ca>
2018-11-09 16:04:27 -0500

    Added simulator project

---
+++ sim/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
