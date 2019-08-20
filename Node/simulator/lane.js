ffb218c4078afd0e09214d834df955220e4af651
Ahmed Awadallah <aawadall@ualberta.ca>
2019-08-20 15:53:52 -0400

    it is not a goo idea to port as is

---
+++ Node/simulator/lane.js
@@ -0,0 +1,11 @@
+/**
+ * Lane
+ */
+class Lane {
+    constructor(name, direction) {
+        this.name = name;
+        this.direction = direction;
+    } 
+}
+
+module.exports = Lane
---
+++ Node/tests/lane.spec.js
@@ -0,0 +1,12 @@
+const expect = require('chai').expect;
+const should = require('chai').should;
+
+describe('Lane', function(){
+    this.beforeEach('Lane Loader', function(){
+
+    })
+
+    it('Should have a direction')
+    it('Should Simulate Flow')
+    it('Should Measure Wait time')
+})
