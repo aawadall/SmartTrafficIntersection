3fa0e9b5b63141ee4e3e7ab04f69322d883c253c
Ahmed Awadallah <aawadall@ualberta.ca>
2019-08-25 21:27:17 -0400

    properties tests pass

--- Node/test/source.spec.js
+++ Node/test/source.spec.js
@@ -1,6 +1,7 @@
 const Source = require('../simulator/source');
 const chai = require('chai');
 chai.use(require('chai-events'));
+chai.use(require('chai-uuid'));
 const should = chai.should();
 
 describe('Source', function () {
@@ -15,11 +16,21 @@ describe('Source', function () {
         it('Should have an id', function () {
             source.should.have.property('id');
         });
-        it('Should have a name');
-        it('Should have an outFlux');
-        it('Should have the id formatted as uuid/v1');
-        it(`Should have its name equal to ${name}`);
-        it(`Should have its outFlux equal to ${outFlux}`);
+        it('Should have a name', function(){
+            source.should.have.property('name');
+        });
+        it('Should have an outFlux', function () {
+            source.should.have.property('outFlux');
+        });
+        it('Should have the id formatted as uuid/v1', function(){
+            source.id.should.be.a.uuid('v1');
+        });
+        it(`Should have its name equal to ${name}`, function(){
+            source.name.should.equal(name);
+        });
+        it(`Should have its outFlux equal to ${outFlux}`, function(){
+            source.outFlux.should.equal(outFlux);
+        });
     });
     describe('Behaviour', function () {
         it('Should generate a flux');
