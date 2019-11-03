a88cb5a3b873d1792d2250df1027e360684a3ae8
Ahmed Awadallah <aawadall@ualberta.ca>
2019-11-03 11:24:01 -0500

    STIN-6: cell interface
    defines contract for cell behavior

--- Simulator/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Simulator
-{
-    public class Class1
-    {
-    }
-}
---
+++ Simulator/MapGrid.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Simulator
+{
+    /// <summary>
+    /// A Map grid is a set of MapCells
+    /// </summary>
+    class MapGrid
+    {
+    }
+}
